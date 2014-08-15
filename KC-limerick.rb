b18c577fea46df82296b6ccbd0f532dde7646ba0
KC Erb <iamkcerb@gmail.com>
2014-08-14 20:05:54 -0600

    fixed typo, added title

--- KC-limerick.rb
+++ KC-limerick.rb
@@ -1,7 +1,10 @@
 Shoes.app do
-  para "KC's an Erb so strong\n",
-       "that one mustn't steep him too long,\n"
-       "or he'll turn such a bitter\n"
-       "and obnoxious critter\n"
-       "you'll know you have done something wrong!"
+  stack margin: 10 do
+    title "KC Limerick"
+    caption "\nKC's an Erb so strong\n",
+    "that one mustn't steep him too long,\n",
+    "or he'll turn such a bitter\n",
+    "and obnoxious critter\n",
+    "you'll know you have done something wrong!"
+  end
 end
