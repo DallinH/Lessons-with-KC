2aa8ee475b475d93a740046cc24712749b1a3253
KC Erb <iamkcerb@gmail.com>
2014-08-14 20:01:05 -0600

    fixed formatting

--- KC-limerick.rb
+++ KC-limerick.rb
@@ -1,3 +1,7 @@
 Shoes.app do
-  para "KC's an Erb so strong that one mustn't steep him too long, or he'll turn such a bitter and obnoxious critter you'll know you have done something wrong"
+  para "KC's an Erb so strong\n",
+       "that one mustn't steep him too long,\n"
+       "or he'll turn such a bitter\n"
+       "and obnoxious critter\n"
+       "you'll know you have done something wrong!"
 end
